91a84b2bd23231349d2d1bdd753eb32fcc84c730
Nicolle Bernal <nibernal04@gmail.com>
2026-05-01 11:21:58 -0500

    Agrego archivo NicolleBernal.py

---
+++ NicolleBernal.py
@@ -0,0 +1,10 @@
+# Información del Estudiante
+
+#Nicolle Bernal Silva
+
+#Programa Academico Ingeniería Ambiental  
+
+#Reflexión breve  
+# La programación permite analizar grandes volúmenes de datos ambientales de forma rápida y precisa. Facilita la simulación de fenómenos como el cambio climático, la calidad del aire o el comportamiento del agua. Además, ayuda a automatizar procesos de monitoreo y control, mejorando la toma de decisiones. De esta manera, se convierte en una herramienta clave para desarrollar soluciones sostenibles y eficientes.
+
+print("Como futuro ingeniero ambiental, es fundamental proteger la calidad del agua, gestionar adecuadamente los residuos, mitigar el cambio climático, conservar los ecosistemas y promover el uso de energías renovables para un desarrollo sostenible.")
